2cb5dc26ba04dd7428ca71ded186a15554dd8c63
Julien Semaan <jul.semaan@gmail.com>
2015-10-29 09:04:35 -0400

    check event value before working it

--- upstream/app/controllers/events_controller.rb
+++ upstream/app/controllers/events_controller.rb
@@ -3,7 +3,7 @@ class EventsController < ApplicationController
     @events = Event.all.limit(50).order('created_at DESC').paginate(:page => params[:page], :per_page => 10)
 
     @events.each do |event|
-      event.value = event.value.gsub(/\n/, '<br>')    
+      event.value = event.value ? event.value.gsub(/\n/, '<br>') : ""
     end
 
     respond_to do |format|
