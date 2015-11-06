c2f3489786ca6d910aa0a4814cf4db8c8dc28bc6
Derek Wuelfrath <dwuelfrath@inverse.ca>
2015-11-06 16:54:59 -0500

    Using absolute filepath

--- remote/perl/lib/fingerbank/Schema/Local.pm
+++ remote/perl/lib/fingerbank/Schema/Local.pm
@@ -3,6 +3,8 @@ package fingerbank::Schema::Local;
 use Moose;
 use namespace::autoclean;
 
+use fingerbank::FilePath qw($INSTALL_PATH);
+
 extends 'DBIx::Class::Schema';
 
 our $VERSION = "2.0";
@@ -10,7 +12,7 @@ our $VERSION = "2.0";
 __PACKAGE__->load_classes;
 
 __PACKAGE__->load_components(qw/Schema::Versioned/);
-__PACKAGE__->upgrade_directory('../../../db/upgrade/');
+__PACKAGE__->upgrade_directory("$INSTALL_PATH/db/upgrade/");
 
 __PACKAGE__->meta->make_immutable;
 
