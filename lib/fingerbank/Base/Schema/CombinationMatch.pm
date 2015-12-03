8bd01e108e1fcc0b446e908c86c3a2d8eded507f
Julien Semaan <jul.semaan@gmail.com>
2015-12-03 14:30:34 -0500

    fix typo

--- lib/fingerbank/Base/Schema/CombinationMatch.pm
+++ lib/fingerbank/Base/Schema/CombinationMatch.pm
@@ -38,7 +38,7 @@ __PACKAGE__->result_source_instance->view_definition(q{
     case when (dhcp_fingerprint_id = $1 AND dhcp_fingerprint_id != '0') OR ( $1 = 0 AND dhcp_fingerprint_id = 0 )then 2 else 0 END +
     case when (dhcp_vendor_id = $2 AND dhcp_vendor_id != '0') OR ( $2 = '0' AND dhcp_vendor_id = '0')then 2 else 0 END +
     case when (user_agent_id = $3 AND user_agent_id != '0') OR ($3 = '0' AND user_agent_id = '0') then 2 else 0 END +
-    case when (dhcp6_fingeprint_id = $5 AND dhcp6_fingerprint_id != '0') OR ($5 = '0' AND dhcp6_fingerprint_id = '0') then 2 else 0 END +
+    case when (dhcp6_fingerprint_id = $5 AND dhcp6_fingerprint_id != '0') OR ($5 = '0' AND dhcp6_fingerprint_id = '0') then 2 else 0 END +
     case when (dhcp6_enterprise_id = $6 AND dhcp6_enterprise_id != '0') OR ($6 = '0' AND dhcp6_enterprise_id = '0') then 2 else 0 END +
     case when (mac_vendor_id = $4 OR (mac_vendor_id IS NULL AND $4 IS NULL)) then 1 else 0 END
     DESC,
