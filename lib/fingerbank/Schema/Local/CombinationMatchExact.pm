627db34b0214785c3ea99778c1cfac00e6c9ff7c
Julien Semaan <jul.semaan@gmail.com>
2015-12-03 14:07:01 -0500

    fix casing

--- lib/fingerbank/Schema/Local/CombinationMatchExact.pm
+++ lib/fingerbank/Schema/Local/CombinationMatchExact.pm
@@ -19,7 +19,7 @@ __PACKAGE__->result_source_instance->view_definition(q{
     WHERE (dhcp_fingerprint_id = $1 OR dhcp_fingerprint_id='')
         AND (dhcp_vendor_id = $2 OR dhcp_vendor_id='')
         AND (user_agent_id = $3 OR user_agent_id='')
-        AND ((mac_vendor_id = $4 OR mac_vendor_id IS NULL) or mac_vendor_id='')
+        AND ((mac_vendor_id = $4 OR mac_vendor_id IS NULL) OR mac_vendor_id='')
         AND (dhcp6_fingerprint_id = $5 OR dhcp6_fingerprint_id='')
         AND (dhcp6_enterprise_id = $6 OR dhcp6_enterprise_id='')
     ORDER BY
