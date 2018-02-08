c150f2ec2fb7324c1cb5a649439e24fc3111af46
Julien Semaan <jul.semaan@gmail.com>
2018-02-08 08:07:14 -0500

    some perldoc

--- lib/fingerbank/Collector.pm
+++ lib/fingerbank/Collector.pm
@@ -1,5 +1,15 @@
 package fingerbank::Collector;
 
+=head1 NAME
+
+fingerbank::API
+
+=head1 DESCRIPTION
+
+Object oriented module to work with the Fingerbank Collector
+
+=cut
+
 use fingerbank::Config;
 use fingerbank::Util;
 use HTTP::Request;
@@ -14,6 +24,12 @@ has 'port' => (is => 'rw');
 has 'use_https' => (is => 'rw');
 has 'cache' => (is => 'rw', default => sub { fingerbank::NullCache->new });
 
+=head2 new_from_config
+
+Create a new collector client from the configured parameters in fingerbank.conf
+
+=cut
+
 sub new_from_config {
     my ($class) = @_;
     my $Config = fingerbank::Config::get_config();
@@ -23,12 +39,23 @@ sub new_from_config {
     );
 }
 
+=head2 get_lwp_client
+
+Get the LWP client to talk to the collector
+
+=cut
+
 sub get_lwp_client {
-    #TODO: allow control of usage of the configured proxy
     my $ua = fingerbank::Util::get_lwp_client(keep_alive => 1);
     $ua->timeout(2);   # An query should not take more than 2 seconds
     return $ua;
 }
+
+=head2 build_request
+
+Given a verb and a path, build the HTTP::Request  based on the client configuration
+
+=cut
 
 sub build_request {
     my ($self, $verb, $path) = @_;
@@ -45,5 +72,32 @@ sub build_request {
 
     return $req;
 }
+
+=head1 AUTHOR
+
+Inverse inc. <info@inverse.ca>
+
+=head1 COPYRIGHT
+
+Copyright (C) 2005-2018 Inverse inc.
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+USA.
+
+=cut
 
 1;
